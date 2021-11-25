288316727e0276ca714e358ce2742f80208b72e5
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2021-11-25 16:09:21 +0200

    changed project name

---
+++ CountriesOfTheWorld/CountriesOfTheWorld/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  Countries
+//
+//  Created by Volodymyr Seredovych on 23.11.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
