a0b2f415752a2d88bb30034a63523a0f0f588d20
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2021-12-03 13:10:16 +0200

    added added header class

--- CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesHeader.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesHeader.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+class CountriesHeaderView: UITableViewHeaderFooterView {
+    static let identifier = "countriesTableHeader"
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("\(#function) has not been implemented")
+    }
+}
