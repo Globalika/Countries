7b470ebb102ce4214d44bc121807434e26d6442a
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2021-12-03 13:21:09 +0200

    added header label

--- CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesHeader.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesHeader.swift
@@ -11,11 +11,23 @@ import UIKit
 class CountriesHeaderView: UITableViewHeaderFooterView {
     static let identifier = "countriesTableHeader"
 
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Choose a card :)"
+        label.font = .systemFont(ofSize: Constants.headerLabelFont)
+        label.textAlignment = .center
+        return label
+    }()
+
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
     }
 
     required init?(coder: NSCoder) {
         fatalError("\(#function) has not been implemented")
+    }
+
+    private struct Constants {
+        static let headerLabelFont: CGFloat = 25
     }
 }
