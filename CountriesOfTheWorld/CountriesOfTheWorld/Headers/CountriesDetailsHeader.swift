70dba31894a27efb20335e2e035a2b342dfe5650
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2021-12-09 02:11:07 +0200

    added images

--- CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesDetailsHeader.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesDetailsHeader.swift
@@ -9,11 +9,31 @@ import Foundation
 import UIKit
 
 class CountriesDetailsHeader: UIView {
+    let worldImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = Constants.worldImage
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    let flagsImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = Constants.flagsImage
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
- 
+
     required init?(coder: NSCoder) {
         fatalError("\(#function) has not been implemented")
+    }
+
+    private struct Constants {
+        static let worldImage = UIImage(named: "world")
+        static let flagsImage = UIImage(named: "flags")
     }
 }
