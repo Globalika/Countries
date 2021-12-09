394e40b4c5c1403a2a78fb2105406ec21e809f3a
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2021-12-09 03:22:28 +0200

    added placeholder properties constraints

--- CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsFieldPlaceHolder.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsFieldPlaceHolder.swift
@@ -49,8 +49,53 @@ class PlaceHolderView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func setVerticalStackConstraints() {
+        NSLayoutConstraint.activate([
+            verticalStack.topAnchor.constraint(equalTo: self.topAnchor),
+            verticalStack.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            verticalStack.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            verticalStack.widthAnchor.constraint(equalToConstant: Constants.verticalStackWidth)
+        ])
+    }
+
+    func setLabelConstraints() {
+        NSLayoutConstraint.activate([
+            tLable.topAnchor.constraint(equalTo: self.topAnchor,
+                                        constant: Constants.labelTopInset),
+            tLable.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            tLable.leadingAnchor.constraint(equalTo: verticalStack.trailingAnchor),
+            tLable.heightAnchor.constraint(equalToConstant: Constants.labelHeight)
+        ])
+    }
+
+    func setCircleImageConstraints() {
+        NSLayoutConstraint.activate([
+            circleImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            circleImageView.leadingAnchor.constraint(equalTo: verticalStack.leadingAnchor),
+            circleImageView.widthAnchor.constraint(equalToConstant: Constants.circleWidth),
+            circleImageView.heightAnchor.constraint(equalToConstant: Constants.circleHeight)
+        ])
+    }
+
+    func setCurveLineImageConstraints() {
+        NSLayoutConstraint.activate([
+            curveLineImageView.topAnchor.constraint(equalTo: circleImageView.bottomAnchor,
+                                                    constant: Constants.curveLineTopInset),
+            curveLineImageView.leadingAnchor.constraint(equalTo: verticalStack.leadingAnchor),
+            curveLineImageView.widthAnchor.constraint(equalToConstant: Constants.curveLineWidth),
+            curveLineImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+
     private struct Constants {
         static let labelAllowedLines = 3
         static let labelMinimumScaleFactor: CGFloat = 0.5
+        static let verticalStackWidth: CGFloat = 80
+        static let labelTopInset: CGFloat = 10
+        static let labelHeight: CGFloat = 35
+        static let circleWidth: CGFloat = 50
+        static let circleHeight: CGFloat = 50
+        static let curveLineTopInset: CGFloat = -10
+        static let curveLineWidth: CGFloat = 50
     }
 }
