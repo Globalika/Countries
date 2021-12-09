b3fa51ac55fd5d9f56e8fb79e7839bc9e4b6a6ef
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2021-12-09 02:06:45 +0200

    created class

---
+++ CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesDetailsHeader.swift
@@ -0,0 +1,19 @@
+//
+//  CountriesDetailsHeader.swift
+//  CountriesOfTheWorld
+//
+//  Created by Volodymyr Seredovych on 09.12.2021.
+//
+
+import Foundation
+import UIKit
+
+class CountriesDetailsHeader: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+ 
+    required init?(coder: NSCoder) {
+        fatalError("\(#function) has not been implemented")
+    }
+}
