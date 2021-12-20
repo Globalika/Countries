cc3e85a930b0eca39fc7e38b16898bb13dd35fc4
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2021-12-20 13:41:11 +0200

    added properties

--- CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsContentForIPhoneView.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsContentForIPhoneView.swift
@@ -6,7 +6,40 @@
 //
 
 import Foundation
+import UIKit
 
 class DetailsContentForIPhoneView {
-    
+    var header: CountriesDetailsHeader = {
+        var header = CountriesDetailsHeader()
+        header.contentMode = .scaleAspectFit
+        header.translatesAutoresizingMaskIntoConstraints = false
+        return header
+    }()
+
+    var flagImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.showsHorizontalScrollIndicator = false
+        return view
+    }()
+
+    var stackView: UIStackView = {
+        var stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = Constants.stackSpacing
+        stack.distribution = .fillEqually
+        return stack
+    }()
+
+    private struct Constants {
+        static let stackSpacing: CGFloat = 5
+    }
 }
