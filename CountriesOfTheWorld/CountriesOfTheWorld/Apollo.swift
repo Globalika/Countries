70deda04caf2d550cf698054c7d47e72ddbadb23
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2022-01-01 17:40:53 +0200

    temp

--- CountriesOfTheWorld/CountriesOfTheWorld/Apollo.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Apollo.swift
@@ -9,19 +9,19 @@ import Apollo
 import Foundation
 
 protocol ApolloClientProtocol {
-    func getCountries(completion: @escaping (Result<[CountriesQuery.Data.Country], Error>) -> Void)
+    func getCountries(completion: @escaping (Result<[CountryLite], Error>) -> Void)
     func getCountry(code: String,
                     completion: @escaping (Result<CountryQuery.Data.Country, Error>) -> Void)
 }
 
 class Apollo: ApolloClientProtocol {
-    func getCountries(completion: @escaping (Result<[CountriesQuery.Data.Country], Error>) -> Void) {
+    func getCountries(completion: @escaping (Result<[CountryLite], Error>) -> Void) {
         let query = CountriesQuery()
         apollo?.fetch(query: query) { result in
             switch result {
             case .success(let graphQLResult):
                 if let data = graphQLResult.data {
-                    completion(Result.success(data.countries))
+                    completion(Result.success(data.countries.compactMap({ CountryLite(country: $0) })))
                 } else if let errors = graphQLResult.errors {
                     print(errors)
                 }
@@ -54,4 +54,66 @@ class Apollo: ApolloClientProtocol {
         }
         return ApolloClient(url: url)
     }()
+}
+
+class CountryLite {
+    public let code: String
+    public let name: String
+    public let capital: String?
+    public let continent: Continent
+    
+    init(code: String,
+         name: String,
+         capital: String?,
+         continent: Continent) {
+        self.code = code
+        self.name = name
+        self.capital = capital
+        self.continent = continent
+    }
+    
+    convenience init?(country: CountriesQuery.Data.Country?) {
+        guard
+            let code = country?.code,
+            let name = country?.name,
+            let capital = country?.capital,
+            let continent = country?.continent
+        else {
+            return nil
+        }
+        self.init(code: code,
+                  name: name,
+                  capital: capital,
+                  continent: Continent(name: continent.name))
+    }
+}
+
+class Continent {
+    public let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    convenience init?(country: Continent?) {
+        guard let name = country?.name
+        else {
+            return nil
+        }
+        self.init(name: name)
+    }
+}
+
+class Language {
+    public let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    convenience init?(country: Language?) {
+        guard let name = country?.name
+        else {
+            return nil
+        }
+        self.init(name: name)
+    }
 }
