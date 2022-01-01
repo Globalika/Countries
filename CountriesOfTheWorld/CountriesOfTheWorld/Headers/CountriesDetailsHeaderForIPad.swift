72d3dbcfcfa344750ba3afe9958d30a8f54be9cf
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2022-01-01 17:56:25 +0200

    created class

---
+++ CountriesOfTheWorld/CountriesOfTheWorld/Headers/CountriesDetailsHeaderForIPad.swift
@@ -0,0 +1,13 @@
+//
+//  CountriesDetailsHeaderForIPad.swift
+//  CountriesOfTheWorld
+//
+//  Created by Volodymyr Seredovych on 01.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class CountriesDetailsHeaderForIPad: UIView {
+    
+}
