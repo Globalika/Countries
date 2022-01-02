1fc419724d2055aff8ed8e5678ae943854ae7d71
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2022-01-02 13:04:16 +0200

    added properties and inits

--- CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsContentForIPadView.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsContentForIPadView.swift
@@ -9,4 +9,26 @@ import Foundation
 import UIKit
 
 class DetailsContentForIPadView: UIView {
+    let worldImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = Constants.worldImage
+        return view
+    }()
+    var countryInfo = [(String, String)]()
+    var points: [CGPoint] = []
+    let curveLinesRotationAngles: [CGFloat] = [0, 90, 180, 0, 90, 180]
+
+    init(countryInfo: [(String, String)], frame: CGRect) {
+        super.init(frame: frame)
+        self.countryInfo = countryInfo
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    private struct Constants {
+        static let worldImage = UIImage(named: "world")
+        static let radius: CGFloat = 150
+    }
 }
