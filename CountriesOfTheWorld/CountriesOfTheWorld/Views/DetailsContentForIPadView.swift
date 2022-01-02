f4ceec4bb8d7540899ca12c595eefd3178226aa9
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2022-01-02 13:33:46 +0200

    added labels configure

--- CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsContentForIPadView.swift
+++ CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsContentForIPadView.swift
@@ -14,13 +14,21 @@ class DetailsContentForIPadView: UIView {
         view.image = Constants.worldImage
         return view
     }()
-    var countryInfo = [(String, String)]()
+
+    var countryInfo = [(String, String)]() {
+        didSet {
+            configureDetailsView()
+        }
+    }
+
     var points: [CGPoint] = []
     let curveLinesRotationAngles: [CGFloat] = []
+    var pointsAmountIncludeScenery: Int = 0
 
     init(countryInfo: [(String, String)], frame: CGRect) {
         super.init(frame: frame)
         self.countryInfo = countryInfo
+        countPointsPositions()
     }
 
     required init?(coder: NSCoder) {
@@ -28,7 +36,7 @@ class DetailsContentForIPadView: UIView {
     }
 
     func countPointsPositions() {
-        let pointsAmountIncludeScenery = countryInfo.count * 2
+        pointsAmountIncludeScenery = countryInfo.count * 2
         let angle: CGFloat = CGFloat(360 / pointsAmountIncludeScenery)
         var point = CGPoint()
         var pointAngle: CGFloat = 0
@@ -48,7 +56,7 @@ class DetailsContentForIPadView: UIView {
             if index%2 == 0 {
                 view.image = UIImage(named: "curveLine")
                 let scale = CGAffineTransform(scaleX: 2.7, y: 2.7)
-                let rotate = CGAffineTransform(rotationAngle: 0) //curveLinesRotationAngles[index/2])
+                let rotate = CGAffineTransform(rotationAngle: 0)
                 view.transform = scale.concatenating(rotate)
             } else {
                 view.image = UIImage(named: "\(scenery)")
@@ -58,8 +66,38 @@ class DetailsContentForIPadView: UIView {
         }
     }
 
+    func addLbels() {
+        if !countryInfo.isEmpty {
+            for (index, point) in points.enumerated() where index % 2 != 0 {
+                let pointAngle = Double(360 / pointsAmountIncludeScenery * (index))
+                let label = UILabel()
+                label.lineBreakMode = .byTruncatingTail
+                label.numberOfLines = 3
+                label.minimumScaleFactor = 0.5
+                label.adjustsFontSizeToFitWidth = true
+                label.setAttributedText(descriptionText: countryInfo[index/2].0,
+                                        descriptionTextFont: .systemFont(ofSize: Constants.labelDescriptionFontSize,
+                                                                         weight: Constants.labelDescriptionFontWeight),
+                                        dataText: countryInfo[index/2].1,
+                                        dataTextFont: .systemFont(ofSize: Constants.labelDataFontSize,
+                                                                  weight: Constants.labelDataFontWeight))
+                addSubview(label)
+                let xadd: CGFloat = round(cos(Double(pointAngle/180)*Double.pi)*75)
+                let yadd: CGFloat = round(sin(Double(pointAngle/180)*Double.pi)*55)
+                label.frame = CGRect(x: point.x + xadd,
+                                     y: point.y - yadd,
+                                     width: 120,
+                                     height: 70)
+            }
+        }
+    }
+
     private struct Constants {
         static let worldImage = UIImage(named: "world")
         static let radius: CGFloat = 150
+        static let labelDescriptionFontSize: CGFloat = 15
+        static let labelDescriptionFontWeight: UIFont.Weight = .thin
+        static let labelDataFontSize: CGFloat = 20
+        static let labelDataFontWeight: UIFont.Weight = .bold
     }
 }
