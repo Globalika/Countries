984c93e9f26015b40bd01067faeda78137ae3833
Volodymyr Seredovych <volodymyr.seredovych@gmail.com>
2022-01-02 12:50:28 +0200

    created class

---
+++ CountriesOfTheWorld/CountriesOfTheWorld/Views/DetailsContentForIPadView.swift
@@ -0,0 +1,12 @@
+//
+//  DetailsContentForIPadView.swift
+//  CountriesOfTheWorld
+//
+//  Created by Volodymyr Seredovych on 02.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class DetailsContentForIPadView: UIView {
+}
